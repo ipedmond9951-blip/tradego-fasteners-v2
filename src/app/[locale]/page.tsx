54b2dfb3f6258d76211f018d8178794d08da2bf2
TradeGo Fasteners <tradego@fasteners.com>
2026-04-16 07:35:54 +0800

    Add TeamCard component between Hero and Products

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,6 +5,7 @@ import AboutSection from '@/components/AboutSection'
 import ProductGrid from '@/components/ProductGrid'
 import FAQSection from '@/components/FAQSection'
 import InquiryForm from '@/components/InquiryForm'
+import TeamCard from '@/components/TeamCard'
 import { GeoPromotion } from '@/components/GeoContent'
 
 export function generateStaticParams() {
@@ -53,6 +54,7 @@ export default async function LocalePage({ params }: { params: Promise<{ locale:
         <GeoPromotion />
       </div>
 
+      <TeamCard locale={locale} />
       <AboutSection locale={locale} />
       <ProductGrid locale={locale} />
       <FAQSection locale={locale} />
